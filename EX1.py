95291ad70014ff4b5e283d6bdd2d192bb4b59748
thuydungg.0 <duongtd05.ptit@gmail.com>
2025-09-26 01:40:22 +0700

    Create EX1.py

---
+++ EX1.py
@@ -0,0 +1,10 @@
+name="Tran Dang Duong "
+old=18
+lop="D25 UDU-05"
+phone="0369222520"
+email="duongtd05.ptit@gmail.com"
+print("Xin chao toan the D25")
+print("Chuc mung tat ca cac ban da thi do vao PTIT")
+print("Minh ten la "+name+str(old)+" tuoi "+ "hoc sinh lop "+lop)
+print("Sdt lien he:"+phone)
+print("Email lien he:"+email)
